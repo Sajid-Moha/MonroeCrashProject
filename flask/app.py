2dbcff790b142897edc3aae9e8d05ec03c0de6a5
Sajid Mohammad <sajidmoha03@gmail.com>
2024-03-09 12:07:03 -0800

    Initialize basic Flask app to be modified

---
+++ flask/app.py
@@ -0,0 +1,59 @@
+# Flask : the app that creates web environment
+# url_for : returns path to files & site path to function
+# render_template: provide jinja template for use as return value
+from flask import Flask, url_for, render_template
+
+# escape : prevents code injection into returned html
+from markupsafe import escape
+
+# request : get data from client
+# redircet : transfer client to other URL
+from flask import request, redirect
+
+from flask import session, flash, get_flashed_messages
+
+app = Flask(__name__)
+app.secret_key = 'doesntmatter'
+
+@app.route('/', methods=['GET'])
+def Home():
+    return render_template('home.html')
+
+@app.route('/processing/', methods=['POST'])
+def ProcessResults():
+    try:
+      SetLatLong(request.form.get('Latitude'),
+                  request.form.get('Longitude'))
+    except:
+      return redirect( url_for('Home') )
+    
+    return redirect( url_for('Results') )
+
+@app.route('/results/')
+def Results():
+    try:
+      latitude = session['lat']
+      longitude = session['long']
+    except:
+       return redirect( url_for('Home') )       
+    
+    return f'Results Page For Latitude {escape(latitude)} and Longitude {escape(longitude)}'
+    
+
+def SetLatLong(latitude, longitude):
+    error = False
+
+    try:
+      session['lat'] = float(latitude)
+    except:
+      flash(f'Error converting value of Latitude ({latitude}) to a float', 'error')
+      error = True
+    
+    try:
+      session['long'] = float(longitude)
+    except:
+      flash(f'Error converting value of Longitude ({longitude}) to a float', 'error')
+      error = True
+
+    if error:
+      raise('invalid latitude or longitude')
